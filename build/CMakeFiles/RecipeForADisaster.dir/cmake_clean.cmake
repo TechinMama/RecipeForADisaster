3e38205c1660aa80e172f94bdeefc192dd2e8504
Alexandra McCoy <mccoyale@gmail.com>
2024-10-12 09:47:08 -0400

    CMake build files

---
+++ build/CMakeFiles/RecipeForADisaster.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RecipeForADisaster.dir/src/main.cpp.o"
+  "CMakeFiles/RecipeForADisaster.dir/src/main.cpp.o.d"
+  "CMakeFiles/RecipeForADisaster.dir/src/recipe.cpp.o"
+  "CMakeFiles/RecipeForADisaster.dir/src/recipe.cpp.o.d"
+  "CMakeFiles/RecipeForADisaster.dir/src/recipeManager.cpp.o"
+  "CMakeFiles/RecipeForADisaster.dir/src/recipeManager.cpp.o.d"
+  "RecipeForADisaster"
+  "RecipeForADisaster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RecipeForADisaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
